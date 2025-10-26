81ce7d7b393ccb358582b3783e46fa5d8a03c0a0
lezi-fun <roy20130108@163.com>
2025-10-26 17:42:01 +0800

    自动提交: 2025-10-26 17:42:00

---
+++ luogu/P8814.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+int main()
+{
+    int k;
+    cin >> k;
+    while(k--)
+    {
+        int n,e,d;
+        cin >> n >> e >> d;
+        
+    }
+    return 0;
+}
