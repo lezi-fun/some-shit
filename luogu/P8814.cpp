1c0629af46ffece46da4b4d1430ed8f690784466
lezi-fun <roy20130108@163.com>
2025-10-26 17:48:01 +0800

    自动提交: 2025-10-26 17:48:01

--- luogu/P8814.cpp
+++ luogu/P8814.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <map>
 using namespace std;
+//n=p*q
+//e*d=(p-1)(q-1)+1
 int main()
 {
     int k;
@@ -11,7 +13,18 @@ int main()
     {
         int n,e,d;
         cin >> n >> e >> d;
-        
+        bool flag = false;
+        for (int p = 1 ; p <= 10000 ; p++)
+        {
+            int q = n / p;
+            if (p * q == n && (e * d == (p - 1) * (q - 1) + 1))
+            {
+                cout << p << " " << q << endl;
+                flag = true;
+                break;
+            }
+        }
+        if (flag == false) cout << "NO" << endl;
     }
     return 0;
 }
