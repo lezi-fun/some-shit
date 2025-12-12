afbe4516ec4aff84b92bb9b3b0d21bb8ab03646d
Auto Sync - mac-mini <roy20130108@126.com>
2025-12-12 20:15:08 +0800

    智能同步: 2025-12-12 20:15:08 - 设备: mac (macos)

--- luogu/杂七杂八.cpp
+++ luogu/杂七杂八.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
-    int a[1000005] = {};
-    for (int i = 1 ; i <= n  ; i++)
+    string s;
+    cin >> s;
+    for (int i = 0 ; i < s.size(); i++)
     {
-        cin >> a[i];
+        if(s[i] != 'z')cout << s[i];
+        else cout << 'a';
     }
-    int ans = 1;
-    int cnt = 0;
-    for (int i = 2 ; i <= n ; i++)
-    {
-        if(a[i] == a[i - 1] + 1)cnt++;
-        else cnt = 0;
-        ans = max(ans , cnt + 1);
-    }
-    cout << ans;
     return 0;
 }
