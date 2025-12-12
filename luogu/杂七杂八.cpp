e45c2a1d7b4c3832e481a727d25634add9f1ba03
Auto Sync - mac-mini <roy20130108@126.com>
2025-12-12 20:25:09 +0800

    智能同步: 2025-12-12 20:25:09 - 设备: mac (macos)

--- luogu/杂七杂八.cpp
+++ luogu/杂七杂八.cpp
@@ -7,19 +7,12 @@ int main()
     cin >> n;
     string s;
     cin >> s;
-    // 确保n在0-25范围内，避免n过大导致的问题
     n = n % 26;
     for (int i = 0 ; i < s.size(); i++)
     {
-        // 只处理小写字母，其他字符原样输出
         if (s[i] >= 'a' && s[i] <= 'z')
         {
-            char shifted = (s[i] - 'a' + n) % 26 + 'a';
-            cout << shifted;
-        }
-        else
-        {
-            cout << s[i];
+            cout <<(s[i] - 'a' + n) % 26 + 'a';
         }
     }
     return 0;
