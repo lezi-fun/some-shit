ea61500c6f1f966c7a5a60a6c81a212d7ce978ad
Auto Sync - mac-mini <roy20130108@126.com>
2025-12-12 20:17:08 +0800

    智能同步: 2025-12-12 20:17:08 - 设备: mac (macos)

--- luogu/杂七杂八.cpp
+++ luogu/杂七杂八.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 int main()
 {
+    int n;
+    cin >> n;
     string s;
     cin >> s;
     for (int i = 0 ; i < s.size(); i++)
     {
-        if(s[i] != 'z')cout << s[i];
-        else cout << 'a';
+        if(s[i] + n > 'z')
+            cout << char(s[i] + n - 26);
+        else
+            cout << char(s[i] + n);
     }
     return 0;
 }
