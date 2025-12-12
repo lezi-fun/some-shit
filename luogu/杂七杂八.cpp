dea21cec3e42e0130028e09b3b60d66d7ebcc83a
Auto Sync - mac-mini <roy20130108@126.com>
2025-12-12 19:47:09 +0800

    智能同步: 2025-12-12 19:47:09 - 设备: mac (macos)

---
+++ luogu/杂七杂八.cpp
@@ -0,0 +1,15 @@
+#incluude <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[1000005] = {};
+    for (int i = 1 ; i <= n  ; i++)
+    {
+        cin >> a[i];
+    }
+    int cnt[1000005] = {};
+    
+    return 0;
+}
